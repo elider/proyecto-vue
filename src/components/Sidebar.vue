7ff564d90747bed3e3f030ca075604c8041c433d
ELIDER <elider.ordaz@gmail.com>
2025-06-04 16:15:15 -0600

    Crea componente lista de tareas, Borra componentes autogenerados, Crea rutas

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="sidebar">
+    <h2>Barra lateral</h2>
+    <ul>
+    <li><RouterLink to="/contador">Contador</RouterLink></li>
+    <li><RouterLink to="/lista-de-tareas">Lista de tareas</RouterLink></li>
+    </ul>  </div>
+</template>
+<script setup>
+import { RouterLink } from 'vue-router';
+
+</script>
+<style>
+.sidebar{
+  width: 200px;
+  background-color: aqua;
+  padding: 20px;
+}
+</style>
