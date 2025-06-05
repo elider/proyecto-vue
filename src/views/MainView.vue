7f6aabf5a625991ff1ad85b1e6168b9870563ed4
ELIDER <elider.ordaz@gmail.com>
2025-06-05 11:28:28 -0600

    uso de pinia y de props

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="container">
+    <Header :mensaje="texto"></Header>
     <Sidebar></Sidebar>
 <div class="content">
+  <input type="texto" v-model="texto">
+  {{ texto }}
   <RouterView></RouterView>
 </div>
   </div>
 </template>
 <script setup>
+import Header from '@/components/Header.vue';
 import Sidebar from '@/components/Sidebar.vue';
+import { ref } from 'vue';
+const texto= ref('');
 </script>
 <style>
 .container{
